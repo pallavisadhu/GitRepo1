2e21aa35d580c6659a5e24594a5370c6cd3ce071
pallavisadhu <pallavisadhu@yahoo.com>
2022-12-04 18:45:20 -0600

    added testng test

--- src/test/java/ExampleGit/sampleProj/AppTest.java
+++ src/test/java/ExampleGit/sampleProj/AppTest.java
@@ -1,7 +1,11 @@
 package ExampleGit.sampleProj;
 
+import org.testng.annotations.Test;
+
 public class AppTest{
-	public static void main(String args[]) {
+
+	@Test
+	public void method1() {
 		System.out.println("Hello");
 		}
 }
